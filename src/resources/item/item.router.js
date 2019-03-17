5449b6ddf90aff45c2da4a34542176a31b1a46b5
parinz1234 <parinya.ons@gmail.com>
2019-03-17 20:36:43 +0700

    Add route item for let test pass

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,18 @@ import { Router } from 'express'
 
 const router = Router()
 
+const mockController = (req, res) => res.send({ message: 'hello' })
+
+router
+  .route('/')
+  .get(mockController)
+  .post(mockController)
+
+router
+  .route('/:id')
+  .get(mockController)
+  .put(mockController)
+  .delete(mockController)
+
+
 export default router
